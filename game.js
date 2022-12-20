172c982786ab50442d74bf69a75e8e64c14cdc5d
Jose <gldmz0318@icloud.com>
2022-12-19 19:50:57 -0800

    edited the css for the box design and reset button design

--- game.js
+++ game.js
@@ -1,2 +1,17 @@
-let btnSelect = document.querySelectorAll("square")
-let btnRestart = document.querySelectorAll('reset-button')
+let btnSelect = document.querySelectorAll("square");
+let btnRestart = document.querySelectorAll('reset-button');
+let winOption = [
+    // horizontal wins
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+    // diagnal wins
+        [0,4,8],
+        [2,4,6],
+    // vertical wins
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+    ];
+    let playerX = true;
+    let turnCount = 0;
