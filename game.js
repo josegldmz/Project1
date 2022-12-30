f31f28b4fcc5a99e2c98cad48ea20f2d55e6ed63
Jose <gldmz0318@icloud.com>
2022-12-30 00:45:37 -0800

    created the EventListener for X and O, and created a function to check if the game was won/draw

--- game.js
+++ game.js
@@ -16,14 +16,53 @@ let winOption = [
 let playerX = true;
 let turnCount = 0;
 
+
+
 // increase the number of turnCount so you can keep track of the number of turns
-for (let i = 0; i < turnCount; i++) {
+for (let i = 0; i <= turnCount; i++) {
     turnCount.append(i);
 }
+if(turnCount === 9){
+    console.log("Draw Game");
+}
 
-// if the number of turnCount is 0 or even, playerX will be true and will make an X
-// if the number of turnCouunt is odd, playerX will be false and will make an O
 
+// make the function for the winning arrays
+const winCheck = () => {
+    for (let i of winOption) {
+        let [element1, element2, element3] = [
+            btnSelect[i[0]].innerText,
+            btnSelect[i[1]].innerText,
+            btnSelect[i[3]].innerText,
+        ];
+        if (element1 != ""&& (element2 != "") & (element3 != "")) {
+            if (element1 == element2 && element2 == element3) {
+                winnerFunc(element1);
+            }
+        }
+    }
+};
+
+// the button selector display of X and O
+btnSelect.forEach((element) => {
+    element.addEventListener("click", () => {
+        if (playerX) {
+            playerX = false;
+            element.innerText = "X";
+            element.disabled = true;
+        }
+        else {
+            playerX = true;
+            element.innerText = "O"
+            element.disabled = true;
+        }
+        turnCount +=1;
+        if (count == 9) {
+// create a draw function -- and how will you display draw?
+        }
+        winCheck();
+    })
+})
 
 
 
